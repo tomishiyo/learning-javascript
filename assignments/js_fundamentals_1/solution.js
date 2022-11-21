658e0dde1239094514e321f588b9f58ab675695a
tomishiyo <tomishiyo@gmail.com>
2022-11-20 21:32:48 -0300

    Completed activities of Data Type lecture

---
+++ assignments/js_fundamentals_1/solution.js
@@ -0,0 +1,31 @@
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+// 1
+let country = 'Brasil';
+let continent = 'South America';
+let population = 214000000;
+
+// 2
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/*
+LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+// 1
+let isIsland = false;
+let language;
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
--- assignments/values_and_variables/solution.js
+++
@@ -1,7 +0,0 @@
-let country = 'Brasil';
-let continent = 'South America';
-let population = 214000000;
-
-console.log(country);
-console.log(continent);
-console.log(population);
